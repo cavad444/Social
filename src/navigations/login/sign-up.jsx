88bbd78ebc459e7d13edce71beb336dc7452e2e9
cavad444 <118839594+cavad444@users.noreply.github.com>
2022-12-08 10:58:06 +0400

    idk

--- src/navigations/login/sign-up.jsx
+++ src/navigations/login/sign-up.jsx
@@ -8,11 +8,11 @@ function SignUp(){
         <h1 className='login-login'>Register</h1>
         <form align="center" className="login-form">
             <span className='email-text'>Your email or username</span> 
-            <input onChange={()=>changeUsernameValue()} placeholder='Enter your username' className='login-input login-username' type="text" />
+            <input onChange={(e)=>changeUsernameValue(e)} placeholder='Enter your username' className='login-input login-username' type="text" />
             <span className='password-text'>Your password</span>
-            <input onChange={()=>changePasswordValue()} pl placeholder='Enter 6 caracter or more' className='login-input login-password' type="password" />
+            <input onChange={(e)=>changePasswordValue(e)} pl placeholder='Enter 6 caracter or more' className='login-input login-password' type="password" />
             <span className='password-text'>Repeat password</span>
-            <input onChange={()=>changeEnterPasswordAgainValue()} placeholder='Enter your password again' className='login-input login-password' type="password" />
+            <input onChange={(e)=>changeEnterPasswordAgainValue(e)} placeholder='Enter your password again' className='login-input login-password' type="password" />
             <input onSubmit={addUsers} value='Login' className='login-button' type="submit" />
         </form>
         <p className='create-an-account'>Already have an account?<Link className='sign-up-plz' to='/login'>Sign In</Link></p>
